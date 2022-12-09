b1378fbc97b90f4f5c5af31c3edfff4df876553c
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2022-12-09 12:28:56 +0530

    edit user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,40 @@ class UserController extends Controller
         return view('users.index', compact('users'));
     }
 
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request->input('name')
+        ]);
+
+        return redirect()->route('users.index')->with('status', 'The user added successfully.');
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update([
+            'name' => $request->input('name'),
+            'email' => $request->input('email')
+        ]);
+
+        return redirect()->route('users.index')->with('edit', 'The user edited successfully.');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect()->route('users.index')->with('delete', 'The user deleted successfully.');
+    }
+
 }
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,24 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h5 class="font-bold text-center text-black">Edit User</h5><br>
+                    <form method="POST" action="{{ route('users.update', $user) }}">
+                        @method('PUT')
+                        @csrf
+                        Name:
+                        <br>
+                        <input type="text" name="name" value="{{ $user->name }}" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" required>
+                        <br><br>
+                        Email:
+                        <br>
+                        <input type="text" name="email" value="{{ $user->email }}" class="w-72 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" required>
+                        <br><br>
+                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-sky-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-sky-400 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">Update</button>                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
