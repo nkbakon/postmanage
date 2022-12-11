e1ee4d96c9b14411ff61d3ca21398dc3ad10db07
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2022-12-11 23:58:31 +0530

    fixed vugs in user delete modal

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -72,8 +72,9 @@
                     <span class="sr-only">Close modal</span>
                 </button>
                 <div class="p-6 text-center">
-                    <form method="POST" action="{{ route('users.destroy') }}">
+                    <form method="POST" action="{{ route('users.destroy', 'userid') }}">
                         @csrf
+                        @method('DELETE')
                         <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this data?</h3>
                         <input type="hidden" name="userid"  id="userid">                        
